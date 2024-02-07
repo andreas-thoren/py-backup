bfc9e288f5d779c5f4b7426740817c9caeeb174f
andreas-thoren <82233402+andreas-thoren@users.noreply.github.com>
2024-02-07 23:40:01 +0100

    Update syncers.py

--- py_backup/syncers.py
+++ py_backup/syncers.py
@@ -457,7 +457,7 @@ class DirComparator:
                         if main_entry.is_file(follow_symlinks=False):
                             if compare_entry is None:
                                 self.comparison_dict["unique"].append(main_entry.path)
-                            if compare_entry.is_file(follow_symlinks=False):
+                            elif compare_entry.is_file(follow_symlinks=False):
                                 if not self.files_are_equal(main_entry, compare_entry):
                                     self.comparison_dict["changed"].append(main_entry.path)
 
