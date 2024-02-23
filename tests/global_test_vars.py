7e1626f9fa4abac24ac5775ec5cac8ddc7b6f265
Andreas Thoren <athoren79@yahoo.com>
2024-02-23 16:46:12 +0100

    test paths should work for windows

--- tests/global_test_vars.py
+++ tests/global_test_vars.py
@@ -15,50 +15,50 @@ RESULT_DST_SRC = {
     "dst": {
         FileType.FILE: {
             FileStatus.UNIQUE: {
-                "delete_file.txt",
-                "common_dir/dst_file.txt",
-                "common_dir/common_inner_dir/inner_dst_file.txt",
-                "inner_dst_dir/inner_dst_file",
-                "inner_dst_dir/nested_inner_dst_dir/nested_inner_dst_file",
-                "src_file_dst_dir/.keep",
+                str(Path("delete_file.txt")),
+                str(Path("common_dir/dst_file.txt")),
+                str(Path("common_dir/common_inner_dir/inner_dst_file.txt")),
+                str(Path("inner_dst_dir/inner_dst_file")),
+                str(Path("inner_dst_dir/nested_inner_dst_dir/nested_inner_dst_file")),
+                str(Path("src_file_dst_dir/.keep")),
             },
-            FileStatus.MISMATCHED: {"src_dir_dst_file"},
+            FileStatus.MISMATCHED: {str(Path("src_dir_dst_file"))},
         },
         FileType.DIR: {
-            FileStatus.UNIQUE: {"inner_dst_dir", "inner_dst_dir/nested_inner_dst_dir"},
-            FileStatus.MISMATCHED: {"src_file_dst_dir"},
+            FileStatus.UNIQUE: {str(Path("inner_dst_dir")), str(Path("inner_dst_dir/nested_inner_dst_dir"))},
+            FileStatus.MISMATCHED: {str(Path("src_file_dst_dir"))},
         },
     },
     "src": {
         FileType.FILE: {
             FileStatus.UNIQUE: {
-                "nested_src_dir/nested_src_file.txt",
-                "src_dir_dst_file/.keep",
-                "common_dir/source_file.txt",
-                "common_dir/common_inner_dir/inner_src_file.txt",
+                str(Path("nested_src_dir/nested_src_file.txt")),
+                str(Path("src_dir_dst_file/.keep")),
+                str(Path("common_dir/source_file.txt")),
+                str(Path("common_dir/common_inner_dir/inner_src_file.txt")),
             },
-            FileStatus.MISMATCHED: {"src_file_dst_dir"},
+            FileStatus.MISMATCHED: {str(Path("src_file_dst_dir"))},
         },
         FileType.DIR: {
-            FileStatus.UNIQUE: {"nested_src_dir"},
-            FileStatus.MISMATCHED: {"src_dir_dst_file"},
+            FileStatus.UNIQUE: {str(Path("nested_src_dir"))},
+            FileStatus.MISMATCHED: {str(Path("src_dir_dst_file"))},
         },
     },
     "mutual": {
         FileType.FILE: {
-            FileStatus.CHANGED: {"testfile1.txt"},
+            FileStatus.CHANGED: {str(Path("testfile1.txt"))},
             FileStatus.EQUAL: {
-                "equal_file.txt",
-                "common_dir/common_inner_dir/common_inner_file.txt",
+                str(Path("equal_file.txt")),
+                str(Path("common_dir/common_inner_dir/common_inner_file.txt")),
             },
         },
         FileType.DIR: {
-            FileStatus.NOT_COMPARED: {"common_dir", "common_dir/common_inner_dir"},
+            FileStatus.NOT_COMPARED: {str(Path("common_dir")), str(Path("common_dir/common_inner_dir"))},
         },
         FileType.SYMLINK: {
             FileStatus.NOT_COMPARED: {
-                "common_dir/loop_link",
-                "common_dir/loop_link_windows",
+                str(Path("common_dir/loop_link")),
+                str(Path("common_dir/loop_link_windows")),
             }
         },
     },
